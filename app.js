37f2d69d6d01323c95744addf9359728ed470c79
EugeneAnipa <e.anipa@student.unisi.it>
2023-09-10 16:49:51 -0400

    all

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 const axios = require("axios");
 const session = require("express-session");
 
+const math = require("mathjs");
+
 var keys = require("object-keys");
 
 var assert = require("assert");
@@ -30,11 +32,6 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.use(session({ secret: "Your Secret Key" }));
-
-//const MySQLStore = require("express-mysql-session")(session);
-//app.use(require("flash")());
-
 app.use(
   session({
     secret: "keyboard cat",
@@ -44,80 +41,23 @@ app.use(
 );
 
 var md5 = require("md5");
+
 /*
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-
-app.use(passport.initialize());
-
-app.use(passport.session());  */
-/* back  
-function checkSignIn(request, respond) {
-  if (request.session.user) {
-    respond.render("transaction");
-  } else {
-    var err = new Error("Not logged I!n");
-    console.log(request.session.id, "you");
-    // next(err);
-  }
-}
-      */
 var firstagricdb = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "firstagricdb",
 });
+    */
 
-/*
-const options = {
-  connectionLimit: 10,
-  password: "",
-  user: "root",
-  database: "firstagricdb",
-  host: "local",
-  createDatabaseTable: true,
-};
-
-const pool = mysql.createPool(options);
-
-const sessionStore = new MySQLStore(options);
-
-app.use(
-  session({
-    name: "log",
-    resave: false,
-    saveUninitialized: false,
-    store: sessionStore,
-    secret: "log",
-    cookie: {
-      maxAge: 60 * 1,
-      sameSite: true,
-    },
-  })
-);   */
-
-//LocalStrategy = require("passport-local").Strategy;
-/*
-passport.use(
-  new LocalStrategy(function verify(Email, Password, cb) {
-    firstagricdb.get(
-      "SELECT * FROM Customers WHERE email = ? AND password = ?",
-      [Email, md5(Password)],
-      function (err, user) {
-        if (err) {
-          return cb(err);
-        }
-        if (!user) {
-          return cb(null, false, {
-            message: "Incorrect username or password.",
-          });
-        }
-      }
-    );
-  })
-);
-*/
+var firstagricdb = mysql.createConnection({
+  host: "sql308.infinityfree.com",
+  user: "if0_35005969",
+  password: "xTIZ9xDZElbgs9",
+  database: "if0_35005969_banking_system",
+  port: 3306,
+});
 
 app.get("/", function (request, respond) {
   respond.render("index");
@@ -144,81 +84,42 @@ app.post("/login", function (request, respond) {
 
   var sql1 =
     "SELECT * FROM Customers,transactions WHERE email = ? AND password = ? ";
-  /*
-  var sql2 =
-    "SELECT customers.firstname,customers.middlename,customers.lastname,customers.email,customers.password,customers.country,customers.state,customers.address,customers.zipcode,customers.phone,customers.dob,customers.accountnumber,transactions.id,transactions.amount,transactions.type,transactions.from_to,transactions.description,transactions.balance,transactions.date FROM customers INNER JOIN transactions ON customers.accountnumber=transactions.accountnumber WHERE email = ? AND password = ? "; 
-    */
 
   firstagricdb.query(sql1, [Email, Password], function (err, result) {
     if (result.length <= 0) {
+      // respond.end("Invalid details");
+
       respond.redirect("/login");
     } else {
-      //respond.render("transaction");
+      request.session.Email = Email;
       respond.redirect("/transaction");
-      // passport.authenticate("local");
-      // request.session.user = Email;
-      // respond.redirect("/transaction");
-      /*
-      Object.keys(result).forEach(function (key) {
-        var row = result[key];
-        console.log(row.title);
-
-        respond.render("transaction", {
-          firstname: row.firstname,
-          middlename: row.middlename,
-          lastname: row.lastname,
-          email: row.email,
-          country: row.country,
-          state: row.state,
-          address: row.address,
-          zipcode: row.zipcode,
-          phone: row.phone,
-          dob: row.dob,
-          accountnumber: row.accountnumber,
-
-          tid: row.id,
-          tamount: row.amount,
-          ttype: row.type,
-          tfrom_to: row.fron_to,
-          tdescription: row.description,
-          tbalance: row.balance,
-          tdate: row.tdate,
-        });
-      });                  */
     }
   });
-
-  //respond.redirect("/login");
 });
 
-app.get(
-  "/transaction",
-  /* checkSignIn,*/ function (request, respond) {
-    let Email = Ee[0];
-    let Password = Pp[0];
+app.get("/transaction", function (request, respond) {
+  let Email = Ee[0];
+  let Password = Pp[0];
 
-    // respond.render("transaction", { id: request.session.user.id });
-    /*
-  if (request.isAuthenticated()) {   */
-    firstagricdb.connect(function (err) {
-      //if (err) throw err;
-      //var ID = "";
-      var sql = "SELECT * FROM Customers";
-      var result1 =
-        "SELECT firstname,middlename,lastname,email,country,state,address,zipcode,phone,dob,accountnumber FROM Customers ORDER BY id DESC LIMIT 1";
+  firstagricdb.connect(function (err) {
+    var sql = "SELECT * FROM Customers";
+    var result1 =
+      "SELECT firstname,middlename,lastname,email,country,state,address,zipcode,phone,dob,accountnumber FROM Customers ORDER BY id DESC LIMIT 1";
 
-      var sql2 =
-        "SELECT customers.firstname,customers.middlename,customers.lastname,customers.email,customers.password,customers.country,customers.state,customers.address,customers.zipcode,customers.phone,customers.dob,customers.accountnumber,transactions.id,transactions.amount,transactions.type,transactions.from_to,transactions.description,transactions.balance,transactions.tdate FROM customers INNER JOIN transactions ON customers.accountnumber=transactions.accountnumber WHERE email = ? AND password = ? ";
+    var sql2 =
+      "SELECT customers.firstname,customers.middlename,customers.lastname,customers.email,customers.password,customers.country,customers.state,customers.address,customers.zipcode,customers.phone,customers.dob,customers.accountnumber,transactions.id,transactions.amount,transactions.type,transactions.from_to,transactions.description,transactions.balance,transactions.tdate FROM customers INNER JOIN transactions ON customers.accountnumber=transactions.accountnumber WHERE email = ? AND password = ? ";
 
-      firstagricdb.query(result1, [Email, Password], function (err, result) {
-        if (err) throw err;
-        //console.log(result);
+    firstagricdb.query(result1, [Email, Password], function (err, result) {
+      if (err) throw err;
+      //console.log(result);
 
+      if (request.session.Email) {
         Object.keys(result).forEach(function (key) {
           var row = result[key];
           //console.log(row.firstname, row.type, row.amount, row.id);
 
           respond.render("transaction", {
+            user: request.Email,
             firstname: row.firstname,
 
             middlename: row.middlename,
@@ -231,40 +132,20 @@ app.get(
             phone: row.phone,
             dob: row.dob,
             accountnumber: row.accountnumber,
-            /*
-            tid: row.id,
-            tamount: row.amount,
-            ttype: row.type,
-            tfrom_to: row.from_to,
-            tdescription: row.description,
-            tbalance: row.balance,
-            tdate: row.tdate,            */
           });
         });
-      });
-    });
-    // } else {
-    //  respond.render("login");  back
-    //}
+      } else {
+        respond.render("login");
+      }
 
-    /*
-
-  firstagricdb.connect(function (err) {
-    //if (err) throw err;
-
-    var sql = "SELECT * FROM Customers";
-    var result1 =
-      "SELECT firstname,middlename,lastname,email,country,state,address,zipcode,phone,dob,accountnumber FROM Customers ORDER BY id DESC LIMIT 1";
-
-    firstagricdb.query(result1, function (err, result) {
-      if (err) throw err;
-
+      /*
       Object.keys(result).forEach(function (key) {
         var row = result[key];
-        console.log(row.title);
+        //console.log(row.firstname, row.type, row.amount, row.id);
 
         respond.render("transaction", {
           firstname: row.firstname,
+
           middlename: row.middlename,
           lastname: row.lastname,
           email: row.email,
@@ -276,22 +157,15 @@ app.get(
           dob: row.dob,
           accountnumber: row.accountnumber,
         });
-      });
+      });       */
     });
   });
-
-  */
-  }
-);
+});
 
 app.post("/transaction", function (request, respond) {
   //respond.redirect("/transaction");
 });
-/*  back, i think you have to look for the next fxn 
-app.use("/transaction", function (err, request, respond, next) {
-  console.log(err);
-  respond.redirect("/transaction");
-});       */
+
 app.get("/signup", function (request, respond) {
   respond.render("signup");
 });
@@ -363,34 +237,30 @@ app.get("/transactionsummary", function (resquest, respond, next) {
     }
   });
 });
+app.get("/total", function (request, respond) {
+  let Email = Ee[0];
 
+  firstagricdb.connect(function (err) {
+    var sql =
+      "SELECT sum(transactions.balance) AS Totalbalance FROM customers INNER JOIN transactions ON customers.accountnumber=transactions.accountnumber WHERE customers.email = ? ";
+
+    firstagricdb.query(sql, [Email], function (err, result) {
+      Object.keys(result).forEach(function (key) {
+        var row = result[key];
+        console.log(row.Totalbalance);
+
+        respond.render("total", { totalBalance: row.Totalbalance });
+      });
+    });
+  });
+});
 app.get("/logout", function (request, respond) {
-  //request.session.destroy(function (err) {
-  //respond.redirect("/login");
-  // });
-  //respond.redirect("/index");
-
   request.session.destroy(function () {
     console.log("user logged out");
   });
   respond.redirect("/login");
 });
 
-app.post("/logout", function (request, respond) {
-  //respond.render("logout");
-  /*
-    request.session.destroy(function (err) {
-    if (err) {
-      return respond - redirect("/index");
-    }
-    sessionStore.close();
-    respond.clearCookie("log");
-    respond.redirect("/login");
-  });   */
-
-  request.logout();
-  respond.redirect("/index");
-});
 app.get("/forgotpassword", function (request, respond) {
   respond.render("forgotpassword");
 });
